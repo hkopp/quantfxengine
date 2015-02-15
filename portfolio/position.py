368943f8bee8d2512a7b5cb27c946bc009a0b307
Henning Kopp <henning.kopp@uni-ulm.de>
2015-02-15 15:23:42 +0100

    more documentation for position

--- portfolio/position.py
+++ portfolio/position.py
@@ -8,6 +8,8 @@ class Position(object):
         exposure: exposure
         avg_price: average price for multiple purchases
         cur_price: current price of the whole position
+        profit_base: current profit or loss
+        profit_perc: current profit or loss in percent
     """
     def __init__(
         self, side, market, units, 
@@ -29,13 +31,22 @@ class Position(object):
         return mult * (self.cur_price - self.avg_price)
 
     def calculate_profit_base(self):
+        """
+        calculates the current profit or loss
+        """
         pips = self.calculate_pips()        
         return pips * self.exposure / self.cur_price
 
     def calculate_profit_perc(self):
+        """
+        calculates the current profit or loss in percent
+        """
         return self.profit_base / self.exposure * 100.0
 
     def update_position_price(self, cur_price):
+        """
+        updates the profit attributes of the position
+        """
         self.cur_price = cur_price
         self.profit_base = self.calculate_profit_base()
         self.profit_perc = self.calculate_profit_perc()
